61be76807e945e2304da9c6f7086501a9bfb5f29
Vitalii Samolovskikh <kefir@perm.ru>
2014-06-18 20:28:26 +0600

    AsynchronousMailController

---
+++ grails-app/controllers/AsynchronousMailController.groovy
@@ -0,0 +1,114 @@
+import grails.plugin.asyncmail.AsynchronousMailMessage
+import grails.plugin.asyncmail.MessageStatus
+
+class AsynchronousMailController {
+    static defaultAction = 'list'
+
+    static allowedMethods = [update: 'POST']
+
+    /**
+     * Show all message in table.
+     */
+    def list() {
+        params.max = Math.min(params.max ? params.max.toInteger() : 10, 100)
+        params.sort = params.sort ?: 'createDate'
+        params.order = params.order ?: 'desc'
+        [list: AsynchronousMailMessage.list(params), total: AsynchronousMailMessage.count()]
+    }
+
+    private withMessage(Closure cl) {
+        AsynchronousMailMessage message = AsynchronousMailMessage.get(params.id)
+        if (message) {
+            return cl(message)
+        }
+
+        flash.message = "Message with id ${params.id} not found."
+        flash.error = true
+        redirect(action: 'list')
+    }
+
+    /**
+     * Show message data.
+     */
+    def show() {
+        withMessage { AsynchronousMailMessage message ->
+            return [message: message]
+        }
+    }
+
+    /**
+     * Show form for editing.
+     */
+    def edit() {
+        withMessage { AsynchronousMailMessage message ->
+            return [message: message]
+        }
+    }
+
+    /**
+     * Update message
+     */
+    def update() {
+        withMessage { AsynchronousMailMessage message ->
+            bindData(
+                    message, params,
+                    [include:
+                             [
+                                     'status',
+                                     'beginDate',
+                                     'endDate',
+                                     'maxAttemptsCount',
+                                     'attemptInterval',
+                                     'priority',
+                                     'markDelete'
+                             ]
+                    ]
+            )
+            message.attemptsCount = 0
+            if (!message.hasErrors() && message.save()) {
+                flash.message = "Message ${params.id} was updated."
+                redirect(action: 'show', id: message.id)
+            } else {
+                render(view: 'edit', model: [message: message])
+            }
+        }
+    }
+
+    /**
+     * Abort message sent
+     */
+    def abort() {
+        withMessage { AsynchronousMailMessage message ->
+            if (message.abortable) {
+                message.status = MessageStatus.ABORT
+                if (message.save()) {
+                    flash.message = "Message ${message.id} was aborted."
+                } else {
+                    flash.message = "Can't abort message with id ${message.id}."
+                    flash.error = true
+                }
+            } else {
+                flash.message = "Can't abort message with id ${message.id} and status ${message.status}."
+                flash.error = true
+            }
+            redirect(action: 'list')
+        }
+    }
+
+    /**
+     * Delete message
+     */
+    def delete() {
+        withMessage { AsynchronousMailMessage message ->
+            try {
+                message.delete()
+                flash.message = "Message with id ${message.id} was deleted."
+                redirect(action: 'list')
+            } catch (Exception e) {
+                flash.message = "Can't delete message with id ${message.id}."
+                flash.error = true
+                redirect(action: 'show', id: message.id)
+            }
+        }
+    }
+}
