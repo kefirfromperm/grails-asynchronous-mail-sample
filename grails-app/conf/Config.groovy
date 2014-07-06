c30a258584b0a0b989d1bbabed3bab8f37771705
Vitalii Samolovskikh <kefir@perm.ru>
2014-07-06 14:38:02 +0600

    Using 1.1-RC1

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -1,4 +1,3 @@
-import org.apache.log4j.Level;
 // locations to search for config files that get merged into the main config;
 // config files can be ConfigSlurper scripts, Java properties files, or classes
 // in the classpath in ConfigSlurper format
